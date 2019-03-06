c043d27b89d2c4c5c08337a7d89644b7d6dd4d6d
TBusen <busen@me.com>
2019-03-06 20:31:56 +0100

    added another rout

--- flask_basics/basic.py
+++ flask_basics/basic.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def index():
     return "<h1>I love puppies!</h1>"
 
+@app.route('/information')
+def info():
+    return "<h1> Here is some information</h1>"
+
 
 if __name__ == "__main__":
     app.run()
