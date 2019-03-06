bdba711998542d9e7a9625e7d4cd419202aba51d
TBusen <busen@me.com>
2019-03-06 20:59:33 +0100

    flake8 compliant and new dynamic view

--- flask_basics/basic.py
+++ flask_basics/basic.py
@@ -7,15 +7,22 @@ app = Flask(__name__)
 def index():
     return "<h1>I love puppies!</h1>"
 
+
 @app.route('/information')
 def info():
     return "<h1> Here is some information</h1>"
 
 # dynamic routes
-@app.route('/user/<name>')
-def somename(name):
-    return f"<h1> This is a page for: {name}"
+
+
+@app.route('/puppy_latin/<name>')
+def latinConvert(name):
+    if name[-1].lower() == 'y':
+        puppname = f'{name[:-1].lower()}iful'
+    else:
+        puppname = f'{name[:-1].lower()}y'
+    return f'<h1> My latin name is {puppname} </h1>'
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
