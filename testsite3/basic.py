c7eeace53deed2f437999edf28fe0242bc71585f
Travis Busen <busen_travis_a@cat.com>
2019-03-08 15:53:24 +0100

    rewrote template for control flow of failures

--- testsite3/basic.py
+++ testsite3/basic.py
@@ -16,18 +16,35 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/report')
-def report():
+@app.route('/reports')
+def reports():
     user = request.args.get('username')
 
-    requirements = [lambda user: any(l.isupper() for l in user),  # build all rules to pass
-                    lambda user: any(l.islower() for l in user),
-                    lambda user: user[-1] in string.digits]
+   # requirements = [lambda user: any(l.isupper() for l in user),  # build all rules to pass
+   #                 lambda user: any(l.islower() for l in user),
+   #                 lambda user: user[-1] in string.digits]
+    #
+   # if all(rule(user) for rule in requirements): # apply all the rules
+   #     return render_template('success.html', username=user)
+   # else:
+   #     return render_template('failure.html', username=user)
 
-    if all(rule(user) for rule in requirements): # apply all the rules
-        return render_template('success.html', username=user)
+    failed = list()
+
+    if any(l.isupper() for l in user):
+        upper = True
     else:
-        return render_template('failure.html', username=user)
+        failed.append('You did not have an upper case letter')
+    if any(l.islower() for l in user):
+        lower = True
+    else:
+        failed.append('You did not have a lower case letter')
+    if user[-1] in string.digits:
+        digit = True
+    else:
+        failed.append('Your username did not end with a number')
+
+    return render_template('reports.html', failed=failed)
 
 
 if __name__ == '__main__':
